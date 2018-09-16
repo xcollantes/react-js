3dd07cee2d825b9a4205a8c2796061298e8a7e03
xcollantes <xcollantes@users.noreply.github.com>
2018-09-15 22:13:08 -0700

    Add listeners

--- server.js
+++ server.js
@@ -5,10 +5,26 @@
 
 */
 var http = require("http");
+var events = require("events");
 
+var eEmitter = new eventsEmitter();
+
+var connectHandler = function connected() {
+	console.log("Connected");
+	
+	eEmitter.emit("Data Received");
+}
+
+
+eEmitter.on('connection', connectHandler);
+
+
+
+/**
 http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end("Hello World!! ");
 }).listen(8000);
 
 console.log("Server is running at http://127.0.0.1:8000/");
+*/
