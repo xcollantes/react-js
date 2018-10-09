0b40e2ca5490b7e9ab77541d77081ff6e5d93bc5
xcollantes <xcollantes@users.noreply.github.com>
2018-10-08 21:46:37 -0700

    Update

--- server.js
+++ server.js
@@ -8,14 +8,19 @@
 
 
 var http = require('http');
+var myFunc = require('./myScript.js');
+var url = require('url');
+var fs = require('fs');
+
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    
+	fs.readFile('demo.html', function(err, data){
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+	});
+    res.end(txt);
 }).listen(8080);
-
-
-
 
 
 /*
